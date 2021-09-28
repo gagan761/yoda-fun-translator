363c6fb0d57cd27c70e35a0d7cd3dacf1ca9e28b
Gagan Kumar <gagan761@gmail.com>
2021-09-28 11:20:25 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+
+
+var inputText=document.querySelector("#translate-input");
+var outputText=document.querySelector("#translate-output");
+
+var url = "https://api.funtranslations.com/translate/yoda.json"
+
+function getTranslationUrl(input){
+    return url+"?"+"text="+ input;
+
+}
+
+
+function clickHandler(event){
+console.log(event);
+    var input= inputText.value;
+    fetch(getTranslationUrl(input))
+    .then(response=>response.json())
+    .then(json =>{
+        outputText.innerText= json.contents.translated;
+    })
+
+
+
+}
+
+
+
+var translateButton= document.querySelector("#translate-button");
+
+translateButton.addEventListener("click",clickHandler);
